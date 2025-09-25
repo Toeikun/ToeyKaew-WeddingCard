6ffd90a6a8676012664ac99aef0adfe1085f1ce1
Toeikun <tyewasin_603as@windowslive.com>
2025-09-25 16:46:38 +0700

    แก้ไขขนาด icon

--- src/components/WeddingGift.jsx
+++ src/components/WeddingGift.jsx
@@ -84,12 +84,12 @@ export default function WeddingGift() {
                 onClick={closeModal}
                 className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
               >
-                &times;
+                X
               </button>
               <img
                 src="./images/QRPayment.jpg"
                 alt="QR Code"
-                className="rounded-xl shadow w-full mb-4"
+                className="rounded-xl shadow w-full mt-4 mb-4"
               />
               {/* <a
                 href="./images/QRPayment.jpg"
